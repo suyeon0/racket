b21184540cd338fb4cb1112c8ceab48a7cdd3438
suyeon <1315702@gmail.com>
2023-04-25 00:16:21 +0900

    init

---
+++ src/main/kotlin/com/racket/commerceplatform/CommercePlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.racket.commerceplatform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommercePlatformApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommercePlatformApplication>(*args)
+}
---
+++ src/test/kotlin/com/racket/commerceplatform/CommercePlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.racket.commerceplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommercePlatformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
