f5b80ca28871d19233dafe1be21bba675361612f
suyeon <1315702@gmail.com>
2023-04-29 20:58:48 +0900

    멀티모듈 구성

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
-rootProject.name = "commerce-platform"
+rootProject.name = "racket"
+include("apps")
+include("apps:api-interface")
+findProject(":apps:api-interface")?.name = "api-interface"
+include("apps:api-interface:user")
+findProject(":apps:api-interface:user")?.name = "user"
