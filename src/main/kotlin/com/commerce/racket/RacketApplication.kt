3536b1b523e38cebd324cbce777592b54f3cd0ce
suyeon <1315702@gmail.com>
2023-05-07 00:43:41 +0900

    init commit

---
+++ src/main/kotlin/com/commerce/racket/RacketApplication.kt
@@ -0,0 +1,11 @@
+package com.commerce.racket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RacketApplication
+
+fun main(args: Array<String>) {
+	runApplication<RacketApplication>(*args)
+}
---
+++ src/test/kotlin/com/commerce/racket/RacketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.commerce.racket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RacketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
