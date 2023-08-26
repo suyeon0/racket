87c72a5a59b2f4e97379f996d0f9c994dbb30140
suyeon <1315702@gmail.com>
2023-08-26 22:27:30 +0900

    #12 api -> implementation

--- api-interface/cash-process/cash/build.gradle.kts
+++ api-interface/cash-process/cash/build.gradle.kts
@@ -9,23 +9,12 @@ object Versions {
 }
 
 dependencies {
-    implementation(project(mapOf("path" to ":api-interface:cash-process:cash-domain")))
-    implementation(project(mapOf("path" to ":api-interface")))
-
-    api("org.springframework.boot:spring-boot-starter-web")
-    api("org.springframework.boot:spring-boot-starter-actuator")
-    api("org.springframework.boot:spring-boot-starter-validation")
-    api("org.springframework.boot:spring-boot-starter-data-jpa")
-    api ("com.mysql:mysql-connector-j")
-    api("org.javassist:javassist:3.29.2-GA")
-    api ("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.kafka:spring-kafka")
-    api ("org.springframework.cloud:spring-cloud-starter-openfeign")
     implementation(project(mapOf("path" to ":api-interface:cash-process:cash-domain")))
     implementation(project(mapOf("path" to ":api-interface:api-shared:user")))
     implementation(project(mapOf("path" to ":api-interface:api-shared:payment")))
-    implementation(project(mapOf("path" to ":shared")))
     implementation(project(mapOf("path" to ":shared:util")))
+    implementation(project(mapOf("path" to ":api-interface")))
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
--- consumer-interface/cash-consumer/build.gradle.kts
+++ consumer-interface/cash-consumer/build.gradle.kts
@@ -1,18 +1,10 @@
 dependencies {
     implementation(project(mapOf("path" to ":api-interface")))
-
-    implementation("org.springframework.kafka:spring-kafka")
     implementation(project(mapOf("path" to ":api-interface:cash-process:cash-domain")))
     implementation(project(mapOf("path" to ":api-interface:cash-process:cash")))
+
+    implementation("org.springframework.kafka:spring-kafka")
     implementation("com.h2database:h2")
-
-    api("org.springframework.boot:spring-boot-starter-web")
-    api("org.springframework.boot:spring-boot-starter-actuator")
-    api("org.springframework.boot:spring-boot-starter-validation")
-
-    api ("org.springframework.cloud:spring-cloud-starter-openfeign")
-    api("org.springframework.retry:spring-retry")
-    api("org.springframework:spring-aspects")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
