0a9a76822744f62559cd501a2f7ba9b9b2e8c30d
suyeon <1315702@gmail.com>
2023-09-27 20:14:18 +0900

    #19 테스트 코드 작성중

--- api-interface/cart/build.gradle.kts
+++ api-interface/cart/build.gradle.kts
@@ -1,19 +1,24 @@
-plugins {
-    id("java")
+dependencyManagement {
+    imports {
+        mavenBom ("org.springframework.cloud:spring-cloud-dependencies:2021.0.8")
+    }
 }
 
-group = "com.commerce"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+object Versions {
+    const val h2 = "2.1.214"
 }
 
 dependencies {
-    implementation(project(mapOf("path" to ":api-interface")))
     implementation(project(mapOf("path" to ":api-interface:api-shared:user")))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation(project(mapOf("path" to ":shared:util")))
+    implementation(project(mapOf("path" to ":api-interface")))
+
+    // Test
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.h2database:h2:${Versions.h2}")
+    testImplementation("it.ozimov:embedded-redis:0.7.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
 }
 
 tasks.getByName<Test>("test") {
